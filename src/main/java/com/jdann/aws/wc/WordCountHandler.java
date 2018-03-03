14fe7cf9376f415eba772973be1a3fe6ea9d99f3
Jonathan Dann <nwjon122@gmail.coml>
2018-03-03 15:22:57 -0800

    removed dependency to increase lambda load time

--- src/main/java/com/jdann/aws/wc/WordCountHandler.java
+++ src/main/java/com/jdann/aws/wc/WordCountHandler.java
@@ -1,38 +1,29 @@
 package com.jdann.aws.wc;
 
-import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanResult;
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
-import com.jdann.aws.wc.dto.Request;
-import com.jdann.aws.wc.dto.Response;
-import com.jdann.aws.wc.dto.Word;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class WordCountHandler implements RequestHandler<Request, Response> {
+public class WordCountHandler {
 
-    private AmazonDynamoDBClient client;
-    private DynamoDB dynamoDb;
+    private AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_WEST_2).build();
     private static final String DYNAMODB_TABLE_NAME = "word_totals";
-    private static final Regions REGION = Regions.US_WEST_2;
     private static final String WORD_COUNT_QUEUE = "https://sqs.us-west-2.amazonaws.com/736338261372/word-count";
     private final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
 
-    public Response handleRequest(Request request, Context context) {
-
-        initDynamoDbClient();
+    public Response handler(Request request) {
 
         Response response = new Response();
         getFromDynamoOrQueue(request.getAddress(), response);
@@ -41,9 +32,15 @@ public class WordCountHandler implements RequestHandler<Request, Response> {
 
     private void getFromDynamoOrQueue(String address, Response response) {
 
+        response.setWords(new ArrayList<>());
+
         if (address == null) {
             response.setStatus("Error: Please enter a valid URL.");
-            response.setWords(new ArrayList<>());
+            return;
+        }
+
+        if (!doesURLExist(address)) {
+            response.setStatus("Error: Invalid URL");
             return;
         }
 
@@ -82,27 +79,124 @@ public class WordCountHandler implements RequestHandler<Request, Response> {
                 .withExpressionAttributeValues(attributeValues);
 
         ScanResult result = client.scan(scanRequest);
-        List<Word> words = new ArrayList<>();
 
         //marshall the results into WordTotals
-        result.getItems().forEach(item -> {
-
-            Word word = new Word();
-            item.forEach((k, v) -> {
-                switch (k) {
-                    case "address"   : word.setAddress(v.getS()); break;
-                    case "word"      : word.setWord(v.getS()); break;
-                    case "total"     : word.setTotal(Long.valueOf(v.getN())); break;
-                }
-            });
-            words.add(word);
-        });
-        return words;
+        return result.getItems().stream().map(this::getWordFromItem).collect(Collectors.toList());
     }
 
-    private void initDynamoDbClient() {
-        client = new AmazonDynamoDBClient();
-        client.setRegion(Region.getRegion(REGION));
-        this.dynamoDb = new DynamoDB(client);
+    private Word getWordFromItem(Map<String, AttributeValue> item) {
+        Word word = new Word();
+
+        for (Map.Entry<String, AttributeValue> entry : item.entrySet()) {
+
+            switch (entry.getKey()) {
+                case "address" : word.setAddress(entry.getValue().getS()); break;
+                case "word"    : word.setWord(entry.getValue().getS()); break;
+                case "total"   : word.setTotal(Long.valueOf(entry.getValue().getN())); break;
+            }
+        }
+        return word;
     }
+
+    private boolean doesURLExist(String address) {
+
+        try {
+
+            URL url = new URL(address);
+
+            //check the current URL
+            HttpURLConnection.setFollowRedirects(false);
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            //don't need to get data
+            httpURLConnection.setRequestMethod("HEAD");
+            //some websites don't like programmatic access so pretend to be a browser
+            httpURLConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 (.NET CLR 3.5.30729)");
+            int responseCode = httpURLConnection.getResponseCode();
+            //only accept response code 200
+            return responseCode == HttpURLConnection.HTTP_OK;
+
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public static class Request {
+
+        private String address;
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+    }
+
+    public static class Response {
+
+        private String status;
+
+        private List<Word> words;
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public List<Word> getWords() {
+            return words;
+        }
+
+        public void setWords(List<Word> words) {
+            this.words = words;
+        }
+
+    }
+
+    public static class Word {
+
+        private String address;
+        private String word;
+        private Long total;
+
+        public Word(String address, String word) {
+            this.address = address;
+            this.word = word;
+            this.total = 0L;
+        }
+
+        public Word() {}
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getWord() {
+            return word;
+        }
+
+        public void setWord(String word) {
+            this.word = word;
+        }
+
+        public Long getTotal() {
+            return total;
+        }
+
+        public void setTotal(Long total) {
+            this.total = total;
+        }
+
+        public static Comparator<Word> byTotal = (Word w1, Word w2) -> w2.getTotal().compareTo(w1.getTotal());
+    }
+
 }
